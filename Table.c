27a9794d277a0044e7c6fc753bfd59365d6f149b
DARSHAN PAWAR <dp7878038@gmail.com>
2025-10-08 17:32:15 +0530

    Add files via upload

---
+++ Table.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int t = 20;
+	printf("%d * 1 =%d\n",t,t*1);
+	printf("%d * 2 =%d\n",t,t*2);
+	printf("%d * 3 =%d\n",t,t*3);
+	printf("%d * 4 =%d\n",t,t*4);
+	printf("%d * 5 =%d\n",t,t*5);
+	printf("%d * 6 =%d\n",t,t*6);
+	printf("%d * 7 =%d\n",t,t*7);
+	printf("%d * 8 =%d\n",t,t*8);
+	printf("%d * 9 =%d\n",t,t*9);
+	printf("%d * 10 =%d\n",t,t*10);
+}
