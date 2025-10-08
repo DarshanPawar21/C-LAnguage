25ef08327a3187af19f44fcd58a56d0068532051
DARSHAN PAWAR <dp7878038@gmail.com>
2025-10-08 11:49:59 +0530

    Add files via upload

---
+++ Basic Function.c.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+{
+	printf("    Abount Me\n");
+	printf("----------------------------\n\n");
+	printf("Name: Pawar Darshan Aababhai\n");
+	printf("Age: 17 year old(Runing)\n");
+	printf("Gender:Male\n");
+	printf("Add: Shree Hari Residency Gate no 3,Plot no 146\n");
+	printf("Profession: Full Stack Developer\n");
+	printf("Hobby:Photo-Graphi , Codind , Gaming , Operating\n\n");
+	printf("------------Thank You-------\n");
+		
+	
+}
