08ffecee22a99b5bd709b2ff1bfef724f04b80e9
DARSHAN PAWAR <dp7878038@gmail.com>
2025-10-08 12:22:20 +0530

    Add files via upload

---
+++ Patten.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	printf("Example No = 2\n");
+	printf(" - - - - - \n");
+	printf("|          |\n");	
+	printf("|          | \n");
+	printf("|    NAME  | \n");		
+	printf("|          | \n");	
+	printf("|          | \n");
+	printf(" - - - - - \n\n\n");	
+	printf("Example No = 3\n\n");
+	printf("  - - - \n");
+	printf("|       |\n");
+	printf("N       |\n");
+	printf("A       |\n");
+	printf("M       |\n");
+	printf("E       |\n");
+	printf("|       |\n");
+	printf("  - - -\n\n ");
+	printf("Example No = 4\n\n");
+	printf("*\n");
+	printf("**\n");
+	printf("***\n");
+	printf("**\n");
+	printf("*\n");
+	printf("Example No = 5\n\n");	
+	printf("*\n");
+	printf("*\n");
+	printf("*                               *  *\n");
+	printf("*                             *       *\n");
+	printf("*        *  *               *\n");
+	printf("*      *      *          *\n");
+	printf("*    *          *      *\n");
+	printf("*  *              *  *\n");
+	printf("*\n\n");
+}
